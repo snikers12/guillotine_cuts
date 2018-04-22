6124cb77f7908f6e2766c8f2dfa105e44df9decf
Rustem Garifyanov <rustem.garifyanov@tgtoil.com>
2018-04-22 15:00:56 +0300

    some changes

--- Algoritm.py
+++ Algoritm.py
@@ -1,4 +1,4 @@
-sheet = {
+main_sheet = {
     'x': 0,
     'y': 0,
     'a': 52,
@@ -9,38 +9,58 @@ sheet = {
     'm': None
 }
 
-res = list(sheet)
+main_sheet_square = main_sheet['a'] * main_sheet['b']
+res = list(main_sheet)
 details = list()
+details_square = 0
+
 
 def get_details():
     f = open('details.txt', 'r')
+    global details_square
     for line in f:
         s = line.split()
         details.append({
-            'a': s[0],
-            'b': s[1]
+            'sum': int(s[0]),
+            'a': int(s[1]),
+            'b': int(s[2])
         })
+        details_square += int(s[0]) * int(s[1]) * int(s[2])
 
-def main():
-    pass
+
+def main(res_list, details_list):
+    dets = details_list
+    sheets = get_sheet(res_list)
+    for sheet in sheets:
+        result = False
+        for detail in dets:
+            if detail['sum'] == 0:
+                continue
+            if place_horizontal(sheet, detail) or place_vertical(sheet, detail):
+                if cut_horizontal(sheet, detail) or cut_vertical(sheet, detail):
+                    result = True
+                    break
+        if not result:
+            sheet['det'] = 'n'
+
 
 def get_sheet(act_list):
-    sheets_list = [item for item in act_list if item['det'] == 'l' and item['cut'] is None]
-    if sheets_list:
-        return sheets_list[-1]
-    else:
-        return False
+    return [item for item in act_list if item['det'] == 'l' and item['cut'] is None]
+
 
 def get_detail(details_list):
     if details_list:
-        return details[0]
+        detail = details_list.pop(0)
+        return detail
     else:
         return False
+
 
 def place_horizontal(sheet, detail):
     if sheet['a'] < detail['a'] or sheet['b'] < detail['b']:
         return False
     is_detail(sheet, detail)
+
 
 def place_vertical(sheet, detail):
     if sheet['b'] < detail['a'] or sheet['a'] < detail['b']:
@@ -48,25 +68,23 @@ def place_vertical(sheet, detail):
     detail['a'], detail['b'] = detail['b'], detail['a']
     is_detail(sheet, detail)
 
+
 def is_detail(sheet, detail):
     if sheet['a'] == detail['a'] and sheet['b'] == detail['b']:
         sheet['det'] = details.index(detail)
         main()
 
+
 def cut_horizontal(sheet, detail):
-    if sheet['b'] <= detail['b']:
-        return False
     sheet['cut'] = 0
     sheet['m'] = detail['b']
     main()
 
+
 def cut_vertical(sheet, detail):
-    if sheet['a'] <= detail['a']:
-        return False
     sheet['cut'] = 0
     sheet['m'] = detail['a']
     main()
 
+
 get_details()
-
-
